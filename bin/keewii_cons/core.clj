ca4f3f865a34ae0d3eb2a6f97328aaddd82b3f04
KangWoo Lee <kangwool@usc.edu>
2014-01-24 14:29:50 -0800

    kk
    kk

---
+++ bin/keewii_cons/core.clj
@@ -0,0 +1,30 @@
+
+;2-control vowel, m,y,w in 1st tab
+; xy-plot in 4th tab
+(ns keewii_cons.core
+  (:use [overtone.live]
+        [keewii_cons.toolbox]
+        [keewii_cons.sequence]
+        [keewii_cons.basic_setting]))
+;Sliders for vowel (1st tab)
+(dosync
+ (osc-handle server "/1/fader1" (fn [msg] (silent-alter-vowel-f1 vowel (first (:args msg)))))
+ (osc-handle server "/1/fader2" (fn [msg] (silent-alter-vowel-f2 vowel (first (:args msg))))))
+
+;Buttons for consonant (1st tab)
+(doseq [i (range (.length Consonant-list))]
+  (let [CONSONANT (str2con_conv (Consonant-list i))
+        BUTTON (str "/1/push" (button_re-sort i))]
+    (osc-handle server BUTTON (fn [msg] (Enable-Consonant (first (:args msg)) CONSONANT)))))
+;stop button (1st tab)
+(osc-handle server "/1/push10" (fn [msg] (enable-vowel (first (:args msg)))));vowel only
+(osc-handle server "/1/push11" (fn [msg] (enable-play (first (:args msg)))));play
+(osc-handle server "/1/push12" (fn [msg] (stop)));stop
+
+;xy plot in 4th tab
+(osc-handle server "/4/xy" (fn [msg](println msg) (vowel-xy f1 f2 (seq (:args msg)))))
+(osc-handle server "/4/toggle5" (fn [msg](println msg) (enable-play (first (:args msg))))) ;play&stop toggle
+
+;(osc-close client)
+;(osc-close server) 
+;(stop)
---
+++ src/keewii_cons/savedata.clj
@@ -0,0 +1,20 @@
+(ns keewii_cons.savedata
+  
+  (:use [keewii_cos.toolbox])
+        ) ;f1,f2?? [keewiiVisual.basic_setting :only [F1 F2]
+
+
+
+
+;temp-path (str (cur_dir) "\\temp\\")
+;        recording-info (saveData/save-temp-wav sox-path temp-path session-number)
+;        data-path (init/datapath @all-options)
+;(sox-recorder recording-info)
+;(saveData/save-temp-seq temp-path session-number) ; save current alphabet (right answer)
+;(saveData/save-data-file temp-path data-path session-number)
+
+;(defn save-data-file [path-temp path-data session-number]
+;  (let []
+;    (copy-file (str path-temp (format "%02d" session-number) ".wav") (str path-data "\\wav\\" (format "%02d" session-number) ".wav"))
+;    (copy-file (str path-temp (format "%02d" session-number) ".txt") (str path-data "\\dat\\" (format "%02d" session-number) ".dat")))) 
+
